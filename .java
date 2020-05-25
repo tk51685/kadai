025c501bf35c62422dcf4552507aa82601cf0f2f
tk51685 <55591698+tk51685@users.noreply.github.com>
2020-05-25 20:15:53 +0900

    Update .java

--- .java
+++ .java
@@ -1,25 +1,25 @@
 import java.util.Arrays;
 
 public class kadai {
-
-
 	public static void main(String[] args) {
+
 		int num[] = new int[10];
-		int sum = 0;
-
 		for(int i = 0; i < num.length ; i++ ) {
-
 			num[i] = (int)(Math.random()*10)+1;
+			int sum = 0;
+		        //ここで配列の合計を代入したいですが、方法がわかりません。
+			sum += num[i];
+
+			int ave = (int)sum / num.length;
+			int min = Arrays.stream(num).min().getAsInt();
+			int max = Arrays.stream(num).max().getAsInt();
+
 			System.out.println(num[i]);
+			System.out.println("最大値"+max);
+			System.out.println("最小値"+ min );
+			System.out.println("合計値"+sum);
+			System.out.println("平均値"+ave);
+			//printlnが10回繰り返されてしまいますが、for文からの抜け方が分かりません。
 		}
-
-		int min = Arrays.stream(num).min().getAsInt();
-		System.out.println("最小値"+ min );
-
-		int max = Arrays.stream(num).max().getAsInt();
-		System.out.println("最大値"+max);
-
-		//int ave = sum / num.length;
-		//System.out.println("平均値"+ ave);
 	}
 }
